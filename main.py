e77f63dc38b2af64cc9514f830d953fabaaeb2c9
Nhan Nguyen <41908529+nttnhannho@users.noreply.github.com>
2024-10-30 15:20:14 +0700

    Add middleware (#3)

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from starlette.middleware.gzip import GZipMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+
+allow_hosts = ['localhost', '127.0.0.1']
+minimum_size_in_byte = 1000
+compression_level = 5
 
 app = FastAPI()
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=allow_hosts)
+app.add_middleware(GZipMiddleware, minimum_size=minimum_size_in_byte, compresslevel=compression_level)
 
 
 @app.get('/')
