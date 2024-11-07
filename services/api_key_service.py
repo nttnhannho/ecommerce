c388b60f19c6b7e954e3b94337046e0831fde869
Nhan Nguyen <41908529+nttnhannho@users.noreply.github.com>
2024-11-07 23:41:13 +0700

    Create API key when signing up shop (#33)

--- services/api_key_service.py
+++ services/api_key_service.py
@@ -1,8 +1,18 @@
 from dbs.mongodb import mongodb
+from exceptions.exception import ApiKeyServiceCreateApiKeyException
 from models.api_key import ApiKey
 
 
 class ApiKeyService:
+    @staticmethod
+    async def create_api_key(key, permission):
+        try:
+            api_key = ApiKey(key=key, permission=permission)
+        except Exception:
+            raise ApiKeyServiceCreateApiKeyException
+
+        return api_key
+
     @staticmethod
     def find_by_key(key):
         collection = mongodb[ApiKey.__collection_name__]
